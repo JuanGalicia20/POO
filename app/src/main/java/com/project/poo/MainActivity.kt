f628cd2a14da58385e9b34c78580adaddb0e6a84
JuanGalicia20 <69065800+JuanGalicia20@users.noreply.github.com>
2020-09-01 17:25:02 -0600

    Proyecto base Android Studio

---
+++ app/src/main/java/com/project/poo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.project.poo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var p:prueba = prueba("jona")
+        println(p.nombre)
+    }
+}
---
+++ app/src/main/java/com/project/poo/prueba.kt
@@ -0,0 +1,12 @@
+package com.project.poo
+
+class prueba {
+
+    var nombre:String = ""
+
+    constructor(nombre:String)
+    {
+        this.nombre = nombre
+    }
+
+}
